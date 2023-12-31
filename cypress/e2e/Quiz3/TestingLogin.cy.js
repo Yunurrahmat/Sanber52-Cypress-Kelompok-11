f55ece28b232426539a7cf4444949b7b62c1e868
yunurrahmat <yunurrahmat@gmail.com>
2023-12-31 11:22:50 +0800

    add Testing Login by Yunurrahmat

---
+++ cypress/e2e/Quiz3/TestingLogin.cy.js
@@ -0,0 +1,39 @@
+let data = require('../../fixtures/userData.json')
+import button from '../../support/pageObject/button'
+import toLogin from '../../support/pageObject/toLogin'
+
+describe('Testing Login', () => {
+    it('Login Successfull', () => {
+        cy.goTo('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        button.clickSignIn()
+        toLogin.login(data.validData.validUser, data.validData.validPass)
+        button.clickLogin()
+        cy.get(':nth-child(2) > .greet > .logged-in').should('contain', 'Welcome')
+    })
+    it('Testing Login with Incorrect Password', () => {
+        cy.goTo('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        button.clickSignIn()
+        toLogin.login(data.validData.validUser, data.invalidData.invalidPass)
+        button.clickLogin()
+        cy.get('.message-error > div').should('contain', 'sign-in was incorrect')
+    })
+    it('Testing Login with Incorrect Email', () => {
+        cy.goTo('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        button.clickSignIn()
+        toLogin.login(data.validData.validUser, data.invalidData.invalidUser)
+        button.clickLogin()
+        cy.get('.message-error > div').should('contain', 'sign-in was incorrect')
+    })
+    it('Forget Password', () => {
+        cy.goTo('https://magento.softwaretestingboard.com/') // CUSTOM COMMAND
+        button.clickSignIn()
+        cy.get(toLogin.uname).type(data.validData.validUser)
+        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > .secondary > .action > span').click()
+        cy.get(toLogin.email).type(data.validData.validUser)
+        button.clickConfirm()
+        cy.get('.message-success > div').should('contain', 'will receive an email with a link to reset your password')
+    })
+})
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
---
+++ file1.js
@@ -0,0 +1,10 @@
+//var
+var buah = 'apel'
+buah = "jeruk"
+console.log(buah)
+
+//let
+let hewan
+hewan = 'kucing'
+hewan = 'tikus'
+console.log(hewan)
